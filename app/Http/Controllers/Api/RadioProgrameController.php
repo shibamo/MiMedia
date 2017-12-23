dc915778526f63d8d479e260b554fbe8e5ac4cca
shibamo <qinchao@hotmail.com>
2017-12-23 12:33:10 -0900

    缓存电视电台频道与节目列表到五分钟过期

--- app/Http/Controllers/Api/RadioProgrameController.php
+++ app/Http/Controllers/Api/RadioProgrameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use App\Http\Controllers\ApiController;
 
 use App\RadioChannel;
@@ -14,22 +15,28 @@ class RadioProgrameController extends ApiController
 {
   public function index()
   {
-    $grouped = RadioPrograme::orderBy('date', 'desc')->get()->groupBy(function ($item, $key) {
-      return  RadioChannel::find($item['radioChannelId'])->name;
+    $radioGrouped = Cache::remember('radio-grouped',5, function(){
+      $grouped = RadioPrograme::orderBy('date', 'desc')->get()->groupBy(function ($item, $key) {
+        return  RadioChannel::find($item['radioChannelId'])->name;
+      });
+      return $grouped->toArray();
     });
 
-    return response()->json($grouped->toArray(), Response::HTTP_OK, $this->jsonHeader, JSON_UNESCAPED_UNICODE);
+    return response()->json($radioGrouped, Response::HTTP_OK, $this->jsonHeader, JSON_UNESCAPED_UNICODE);
   }
 
   public function channels()
   {
-    $channels = RadioChannel::all()->makeHidden(['created_at', 'updated_at', 'deleted_at']);
-    $channels->each(function ($item, $key) {
-      $item['lastPrograme'] =  RadioChannel::find($item->id)//这里不能直接用$item,否则输出的数组会带上programes的内容
-        ->radioProgrames->last()->makeHidden([
-          'radioChannelId', 'isChecked', 'isPublished', 'created_at', 'updated_at', 'deleted_at']);
+    $radioChannels = Cache::remember('radio-channels',5, function(){
+      $channels = RadioChannel::all()->makeHidden(['created_at', 'updated_at', 'deleted_at']);
+      $channels->each(function ($item, $key) {
+        $item['lastPrograme'] =  RadioChannel::find($item->id)//这里不能直接用$item,否则输出的数组会带上programes的内容
+          ->radioProgrames->last()->makeHidden([
+            'radioChannelId', 'isChecked', 'isPublished', 'created_at', 'updated_at', 'deleted_at']);
+      });
+      return $channels;
     });
 
-    return response()->json($channels, Response::HTTP_OK, $this->jsonHeader, JSON_UNESCAPED_UNICODE);    
+    return response()->json($radioChannels, Response::HTTP_OK, $this->jsonHeader, JSON_UNESCAPED_UNICODE);    
   }
 }
--- app/Http/Controllers/Api/TvProgrameController.php
+++ app/Http/Controllers/Api/TvProgrameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use App\Http\Controllers\ApiController;
 
 use App\TvChannel;
@@ -14,24 +15,30 @@ class TvProgrameController extends ApiController
 {
   public function index()
   {
-    $grouped = TvPrograme::orderBy('date', 'desc')->get()->makeHidden([
-      'isChecked', 'isPublished', 'created_at', 'updated_at', 'deleted_at'])
-      ->groupBy(function ($item, $key) {
-        return  TvChannel::find($item['tvChannelId'])->name;
+    $tvGrouped = Cache::remember('tv-grouped',5, function(){
+      $grouped = TvPrograme::orderBy('date', 'desc')->get()->makeHidden([
+        'isChecked', 'isPublished', 'created_at', 'updated_at', 'deleted_at'])
+        ->groupBy(function ($item, $key) {
+          return  TvChannel::find($item['tvChannelId'])->name;
+      });
+      return $grouped->toArray();
     });
 
-    return response()->json($grouped->toArray(), Response::HTTP_OK, $this->jsonHeader, JSON_UNESCAPED_UNICODE);
+    return response()->json($tvGrouped, Response::HTTP_OK, $this->jsonHeader, JSON_UNESCAPED_UNICODE);
   }
 
   public function channels()
   {
-    $channels = TvChannel::all()->makeHidden(['created_at', 'updated_at', 'deleted_at']);
-    $channels->each(function ($item, $key) {
-      $item['lastPrograme'] =  TvChannel::find($item->id)//这里不能直接用$item,否则输出的数组会带上tvProgrames的内容
-        ->tvProgrames->last()->makeHidden([
-          'tvChannelId', 'isChecked', 'isPublished', 'created_at', 'updated_at', 'deleted_at']);
+    $tvChannels = Cache::remember('tv-channels',5, function(){
+      $channels = TvChannel::all()->makeHidden(['created_at', 'updated_at', 'deleted_at']);
+      $channels->each(function ($item, $key) {
+        $item['lastPrograme'] =  TvChannel::find($item->id)//这里不能直接用$item,否则输出的数组会带上tvProgrames的内容
+          ->tvProgrames->last()->makeHidden([
+            'tvChannelId', 'isChecked', 'isPublished', 'created_at', 'updated_at', 'deleted_at']);
+      });
+      return $channels;
     });
 
-    return response()->json($channels, Response::HTTP_OK, $this->jsonHeader, JSON_UNESCAPED_UNICODE);
+    return response()->json($tvChannels, Response::HTTP_OK, $this->jsonHeader, JSON_UNESCAPED_UNICODE);
   }
 }
